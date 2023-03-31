cc3d796ee6533165f2a17e0786c59c69f43f99a2
mario <mstroo87@outlook.com>
2023-03-31 17:33:58 +0200

    toevoegen untracked file

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,10 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', include)'blog.urls'))
 ]
